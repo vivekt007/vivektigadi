e030433015d871b8bff93fd57e09a42b92c09b06
vivekt007 <vivektigadi59@gmail.com>
2025-01-08 22:53:40 +0530

    `Update package.json and vite.config.js with minor changes`

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/", // Ensures proper paths for static assets in production
+    base: "./", // Ensures proper paths for static assets in production
     server: {
         port: 3000,
         host: "0.0.0.0",
