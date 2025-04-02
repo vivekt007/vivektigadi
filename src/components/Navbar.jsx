bf8c0ec684cd9a8e0bf84762006e6135eecb2676
Vivek Tigadi <vivektigadi59@gmail.com>
2025-04-03 01:26:05 +0530

    favicon update 3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import Logo from "/logo/favIcon.svg";
+import Logo from "/logo/favicon_transparent.svg";
 
 // Css
 import "../styles/Navbar.css";
@@ -9,46 +9,44 @@ import "../styles/Navbar.css";
 import { observeElements } from "../Javascript/animation.js";
 
 function Navbar() {
-    const [menuOpen, setMenuOpen] = useState(false);
-    const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const navigate = useNavigate();
 
-    const navigateToSection = (path, sectionId) => {
-        navigate(path);
-        setTimeout(() => {
-            const element = document.getElementById(sectionId);
-            if (element) {
-                element.scrollIntoView({ behavior: "smooth" });
-            }
-        }, 100); // Delay for page load/rendering
-    };
+  const navigateToSection = (path, sectionId) => {
+    navigate(path);
+    setTimeout(() => {
+      const element = document.getElementById(sectionId);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }, 100); // Delay for page load/rendering
+  };
 
-    useEffect(() => {
-        observeElements();
-    }, []);
+  useEffect(() => {
+    observeElements();
+  }, []);
 
-    return (
-        <nav className="navbar blur">
-            <div className="logo">
-                <a href="/">
-                    <img src={Logo} alt="Logo" />
-                </a>
-            </div>
+  return (
+    <nav className="navbar blur">
+      <div className="logo">
+        <a href="/">
+          <img src={Logo} alt="Logo" />
+        </a>
+      </div>
 
-            <div className="navbar-links">
-                <li>
-                    <p onClick={() => navigateToSection("/", "heroSection")}>
-                        PROJECTS
-                    </p>
-                </li>
-                <li>
-                    <div className="verticle-line"></div>
-                </li>
-                <li>
-                    <p onClick={() => navigateToSection("/", "about")}>ABOUT</p>
-                </li>
-            </div>
-        </nav>
-    );
+      <div className="navbar-links">
+        <li>
+          <p onClick={() => navigateToSection("/", "heroSection")}>PROJECTS</p>
+        </li>
+        <li>
+          <div className="verticle-line"></div>
+        </li>
+        <li>
+          <p onClick={() => navigateToSection("/", "about")}>ABOUT</p>
+        </li>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
